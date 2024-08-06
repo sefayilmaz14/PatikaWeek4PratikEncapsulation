8e54694978c9d63a8cfff95775867097f66c10eb
sefayilmaz <sefaylmaz14@gmail.com>
2024-08-07 01:48:22 +0300

    Add project files.

---
+++ PatikaWeek4PratikEncapsulation/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PratikEncapsulation
+{
+    public class Car
+    {
+        //Field tanımlamaları
+        private string _trademark;
+        private string _model;
+        private string _color;
+        private int _doorNumber;
+       
+        //Constructor tanımlaması
+        public Car(string trademark, string model, string color, int doorNumber)
+        {
+            Trademark = trademark;
+            Model = model;
+            Color = color;
+            DoorNumber = doorNumber; 
+        }
+        //Propetry tanımlamaları
+        public string Trademark { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+        
+        public int DoorNumber
+        {
+            get
+            {
+                return _doorNumber;
+            }
+            set//Kapsülleme işlemi
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Araba kapı sayısı 2 veya 4 olmalıdır.");
+                    _doorNumber = -1; // Geçersiz bir değer atanabilir
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Kayıdı Oluşturulan arabanın markası {Trademark}, Modeli {Model}, Rengi {Color}, Kapı Sayısı {DoorNumber}");
+        }
+    }
+}
---
+++ PatikaWeek4PratikEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using PatikaWeek4PratikEncapsulation;
+
+ class Program
+{
+     static void Main(string[] args)
+    {
+        Car car1 = new Car("Lada" ,"Niva","Beyaz",4);
+        car1.Display();
+
+        Console.WriteLine("-----------------------------------------------------------");
+
+        Car car2 = new Car("Honda", "S200", "Siyah", 2);
+        car2.Display();
+
+        Console.WriteLine("-----------------------------------------------------------");
+
+        Car car3 = new Car("Renaul", "Laguna", "Kırmızı", 3);
+        car3.Display();
+       
+    }
+
+}
